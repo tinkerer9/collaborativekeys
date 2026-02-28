20e394e3c3915fe50cfd085657d193f5d149ce07
Max Parisi <makermax101@gmail.com>
2026-02-28 12:12:44 -0800

    add files

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const server = http.createServer((req, res) => {
+    // Serve index.html for /
+    let filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end("Error loading index.html");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("keyPress", (data) => {
+        console.log(`Key pressed from ${socket.id}:`, data.key);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
