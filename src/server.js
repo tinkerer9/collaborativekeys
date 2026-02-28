ad0c80c68173e3fafd4fcd08f1df8ea284bb5f0f
Max Parisi <makermax101@gmail.com>
2026-02-28 12:41:39 -0800

    fix server.js

--- src/server.js
+++ src/server.js
@@ -3,15 +3,34 @@ const fs = require("fs");
 const path = require("path");
 const { Server } = require("socket.io");
 
+const publicDir = path.join(__dirname, "public");
+
 const server = http.createServer((req, res) => {
-    // Serve index.html for /
-    let filePath = path.join(__dirname, "public/index.html");
+    // Default to index.html if requesting /
+    let filePath = path.join(publicDir, req.url === "/" ? "index.html" : req.url);
+
+    // Determine content type
+    const extname = path.extname(filePath);
+    let contentType = "text/plain";
+
+    switch (extname) {
+        case ".html": contentType = "text/html"; break;
+        case ".js": contentType = "application/javascript"; break;
+        case ".css": contentType = "text/css"; break;
+        case ".png": contentType = "image/png"; break;
+        case ".jpg": contentType = "image/jpeg"; break;
+        case ".gif": contentType = "image/gif"; break;
+        case ".ico": contentType = "image/x-icon"; break;
+    }
+
+    // Read the file
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            res.writeHead(500);
-            return res.end("Error loading index.html");
+            res.writeHead(404);
+            res.end("File not found");
+            return;
         }
-        res.writeHead(200, { "Content-Type": "text/html" });
+        res.writeHead(200, { "Content-Type": contentType });
         res.end(data);
     });
 });
@@ -22,8 +41,8 @@ io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
     socket.on("keyPress", (data) => {
-        socket.emit("keyPressEcho", `Key pressed from ${socket.id}:`, data.key);
-        console.log(`Key pressed from ${socket.id}:`, data.key);
+        socket.emit("keyPressEcho", `Key pressed from ${socket.id}: <b>${data.key}</b>`); // send to clients
+        console.log(`Key pressed from ${socket.id}: ${data.key}`);
     });
 
     socket.on("disconnect", () => {
